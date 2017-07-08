cee9fdbb30d5a2dd03ef215e5fdee0ee2a758896
tiemonl <tiemon.liam@gmail.com>
2017-07-08 18:17:51 -0400

    Added French translations

--- Items/UpgradeHellstone.cs
+++ Items/UpgradeHellstone.cs
@@ -13,17 +13,17 @@ namespace MagicStorage.Items
 			DisplayName.SetDefault("Hellstone Storage Upgrade");
 			DisplayName.AddTranslation(GameCulture.Russian, "Адское улучшение хранилища");
 			DisplayName.AddTranslation(GameCulture.Polish, "Ulepszenie jednostki magazynującej (Piekielny kamień)");
-            		DisplayName.AddTranslation(GameCulture.French, "Amélioration d'Unité de stockage (Infernale)");
+			DisplayName.AddTranslation(GameCulture.French, "Amélioration d'Unité de stockage (Infernale)");
 
-            		Tooltip.SetDefault("Upgrades Storage Unit to 120 capacity"
+			Tooltip.SetDefault("Upgrades Storage Unit to 120 capacity"
 				+ "\n<right> a Demonite/Crimtane Storage Unit to use");
 			Tooltip.AddTranslation(GameCulture.Russian, "Улучшает блок хранения до 120 вместимости"
-			        + "\n<right> по демонитовому/кримтановому блоку хранения для использования");
+				+ "\n<right> по демонитовому/кримтановому блоку хранения для использования");
 			Tooltip.AddTranslation(GameCulture.Polish, "Ulepsza jednostkę magazynującą do 120 miejsc"
 				+ "\n<right> na Jednostkę magazynującą (Karmazynit/Demonit), aby użyć");
-            		Tooltip.AddTranslation(GameCulture.French, "améliore la capacité de unité de stockage à 120"
-                		+ "\n<right> l'unité de stockage (Démonite/Carmitane) pour utiliser");
-        	}
+			Tooltip.AddTranslation(GameCulture.French, "améliore la capacité de unité de stockage à 120"
+				+ "\n<right> l'unité de stockage (Démonite/Carmitane) pour utiliser");
+		}
 
 		public override void SetDefaults()
 		{
