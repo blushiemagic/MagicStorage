3d7db518480df7b810c110ea8d1eddbc1c5670c3
tiemonl <tiemon.liam@gmail.com>
2017-07-10 11:07:18 -0400

    Added Spanish translations

--- Items/CraftingAccess.cs
+++ Items/CraftingAccess.cs
@@ -13,7 +13,8 @@ namespace MagicStorage.Items
 			DisplayName.SetDefault("Storage Crafting Interface");
 			DisplayName.AddTranslation(GameCulture.Russian, "Интерфейс создания хранилищ");
 			DisplayName.AddTranslation(GameCulture.Polish, "Interfejs Rzemieślniczy Magazynu");
-			DisplayName.AddTranslation(GameCulture.French, "Interface de Stockage Artisanat"); 
+			DisplayName.AddTranslation(GameCulture.French, "Interface de Stockage Artisanat");
+			DisplayName.AddTranslation(GameCulture.Spanish, "Interfaz de Elaboración de almacenamiento");
 		}
 
 		public override void SetDefaults()
