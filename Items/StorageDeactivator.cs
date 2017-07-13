4cc282b712764fdd12f4470311403a90a7847680
tiemonl <tiemon.liam@gmail.com>
2017-07-12 22:41:40 -0400

    added spanish translations

--- Items/StorageDeactivator.cs
+++ Items/StorageDeactivator.cs
@@ -15,21 +15,15 @@ namespace MagicStorage.Items
 			DisplayName.SetDefault("Storage Unit Wand");
 			DisplayName.AddTranslation(GameCulture.Russian, "Посох блока хранения");
 			DisplayName.AddTranslation(GameCulture.Polish, "Różdżka jednostki magazynującej");
-<<<<<<< HEAD
 			DisplayName.AddTranslation(GameCulture.French, "Baguette d'unité de stockage");
 			DisplayName.AddTranslation(GameCulture.Spanish, "Varita de unidad de almacenamiento");
-=======
 			DisplayName.AddTranslation(GameCulture.French, "Baguetter d'unité de stockage");
->>>>>>> upstream/master
 
 			Tooltip.SetDefault("<right> Storage Unit to toggle between Active/Inactive");
 			Tooltip.AddTranslation(GameCulture.Russian, "<right> по Блоку Хранения для переключения между Включенным/Выключенным состоянием");
 			Tooltip.AddTranslation(GameCulture.Polish, "<right> aby przełączyć Jednostkę Magazynującą (wł./wył.)");
 			Tooltip.AddTranslation(GameCulture.French, "<right> pour changer l'unité de stockage actif/inactif");
-<<<<<<< HEAD
 			Tooltip.AddTranslation(GameCulture.Spanish, "<right> para cambiar el unidad de almacenamiento activo/inactivo");
-=======
->>>>>>> upstream/master
 		}
 
 		public override void SetDefaults()
