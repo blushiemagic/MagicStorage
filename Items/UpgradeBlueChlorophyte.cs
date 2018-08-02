8c89626dcafa08bf1562403cf724a307c7526266
Caicyo <42014149+DeltaRune@users.noreply.github.com>
2018-08-02 15:08:00 +0300

    Fixed Russian Translation.

--- Items/UpgradeBlueChlorophyte.cs
+++ Items/UpgradeBlueChlorophyte.cs
@@ -11,15 +11,15 @@ namespace MagicStorage.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Blue Chlorophyte Storage Upgrade");
-			DisplayName.AddTranslation(GameCulture.Russian, "Синее хлорофитовое улучшение хранилища");
+			DisplayName.AddTranslation(GameCulture.Russian, "Синее Хлорофитовое Улучшение Ячейки");
 			DisplayName.AddTranslation(GameCulture.Polish, "Ulepszenie jednostki magazynującej (Niebieski Chlorofit)");
 			DisplayName.AddTranslation(GameCulture.French, "Amélioration d'Unité de stockage (Chlorophylle Bleu)");
 			DisplayName.AddTranslation(GameCulture.Spanish, "Actualización de Unidad de Almacenamiento (Clorofita Azul)");
 
 			Tooltip.SetDefault("Upgrades Storage Unit to 240 capacity"
 				+ "\n<right> a Hallowed Storage Unit to use");
-			Tooltip.AddTranslation(GameCulture.Russian, "Улучшает блок хранения до 240 вместимости"
-				+ "\n<right> по святому блоку хранения для использования");
+			Tooltip.AddTranslation(GameCulture.Russian, "Увеличивает количество слотов в Ячейке Хранилища до 240"
+				+ "\n<right> на Святой Ячейке Хранилища для улучшения");
 			Tooltip.AddTranslation(GameCulture.Polish, "Ulepsza jednostkę magazynującą do 240 miejsc"
 				+ "\n<right> na Jednostkę magazynującą (Święconą), aby użyć");
 			Tooltip.AddTranslation(GameCulture.French, "améliore la capacité de unité de stockage à 240"
