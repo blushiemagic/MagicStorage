dad0df7d968944b1e249392cbd0c789ace1a234b
Liam <tiemon.liam@gmail.com>
2019-10-19 14:01:17 -0400

    Add search improvements (#91)
    * add gitignore and gitattributes
    
    .gitignore will ignore compilation files and .gitattributes will ignore line endings
    
    * refactor getting search rectangle
    
    * add Delete, Left, Right, Home, and End support
    
    * fix Delete to be able to delete last character in a string
    
    * fix Delete to be able to delete last character in a string
    
    * fixed backspace issue.
    
    backspace will now delete the current cursor position and leave the cursor in that position.

--- UISearchBar.cs
+++ UISearchBar.cs
@@ -55,11 +55,12 @@ namespace MagicStorage
 			cursorTimer++;
 			cursorTimer %= 60;
 
+			Rectangle dim = InterfaceHelper.GetFullRectangle(this);
+			MouseState mouse = StorageGUI.curMouse;
+			bool mouseOver = mouse.X > dim.X && mouse.X < dim.X + dim.Width && mouse.Y > dim.Y && mouse.Y < dim.Y + dim.Height;
+
 			if (StorageGUI.MouseClicked && Parent != null)
 			{
-				Rectangle dim = InterfaceHelper.GetFullRectangle(this);
-				MouseState mouse = StorageGUI.curMouse;
-				bool mouseOver = mouse.X > dim.X && mouse.X < dim.X + dim.Width && mouse.Y > dim.Y && mouse.Y < dim.Y + dim.Height;
 				if (!hasFocus && mouseOver)
 				{
 					hasFocus = true;
@@ -72,21 +73,14 @@ namespace MagicStorage
 					cursorPosition = text.Length;
 				}
 			}
-			else if (StorageGUI.curMouse.RightButton == ButtonState.Pressed && StorageGUI.oldMouse.RightButton == ButtonState.Released)
-			{
-				Rectangle dim = InterfaceHelper.GetFullRectangle(this);
-				MouseState mouse = StorageGUI.curMouse;
-				bool mouseOver = mouse.X > dim.X && mouse.X < dim.X + dim.Width && mouse.Y > dim.Y && mouse.Y < dim.Y + dim.Height;
-				if (!mouseOver && Parent != null && hasFocus)
-				{
-					hasFocus = false;
-					cursorPosition = text.Length;
-					CheckBlockInput();
-				}
-				if (mouseOver) {
-					Reset();
-					StorageGUI.RefreshItems();
-					hasFocus = true;
+			else if (StorageGUI.curMouse.RightButton == ButtonState.Pressed && StorageGUI.oldMouse.RightButton == ButtonState.Released && Parent != null && hasFocus && !mouseOver) {
+				hasFocus = false;
+				cursorPosition = text.Length;
+				CheckBlockInput();
+			} else if (StorageGUI.curMouse.RightButton == ButtonState.Pressed && StorageGUI.oldMouse.RightButton == ButtonState.Released && mouseOver) {
+				if (text.Length > 0) {
+					text = string.Empty;
+					cursorPosition = 0;
 				}
 			}
 
@@ -94,19 +88,43 @@ namespace MagicStorage
 			{
 				PlayerInput.WritingText = true;
 				Main.instance.HandleIME();
-				string newString = Main.GetInputText(text);
-				if ((Main.keyState.IsKeyDown(Keys.LeftControl) || Main.keyState.IsKeyDown(Keys.RightControl)) && KeyTyped(Keys.Back))
-				{
-					newString = string.Empty;
-				}
-				if (!newString.Equals(text))
-				{
+				string prev = text;
+
+				if (cursorPosition < text.Length && text.Length > 0)
+					prev = prev.Remove(cursorPosition);
+
+				string newString = Main.GetInputText(prev);
+				bool changed = false;
+
+				if (!newString.Equals(prev)) {
+					int newStringLength = newString.Length;
+					if (prev != text) newString += text.Substring(cursorPosition);
 					text = newString;
-					cursorPosition = text.Length;
-					StorageGUI.RefreshItems();
+					cursorPosition = newStringLength;
+					changed = true;
 				}
-				if (KeyTyped(Keys.Enter) || KeyTyped(Keys.Tab) || KeyTyped(Keys.Escape))
-				{
+
+				if (KeyTyped(Keys.Delete) && text.Length > 0 && cursorPosition <= text.Length - 1) {
+					text = text.Remove(cursorPosition, 1);
+					changed = true;
+				}
+
+				if (KeyTyped(Keys.Left) && cursorPosition > 0)
+					cursorPosition--;
+
+				if (KeyTyped(Keys.Right) && cursorPosition < text.Length)
+					cursorPosition++;
+
+				if (KeyTyped(Keys.Home))
+					cursorPosition = 0;
+
+				if (KeyTyped(Keys.End))
+					cursorPosition = text.Length;
+
+				if (changed)
+					StorageGUI.RefreshItems();
+
+				if (KeyTyped(Keys.Enter) || KeyTyped(Keys.Tab) || KeyTyped(Keys.Escape)) {
 					hasFocus = false;
 					CheckBlockInput();
 				}
