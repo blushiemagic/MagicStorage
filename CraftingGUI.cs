95f17febc8966cde7e38940e11d8871302f0af7e
Exterminatorx99 <kristijan.kos99@gmail.com>
2021-09-16 17:23:00 +0200

    Add config for clearing search text on UI open

--- CraftingGUI.cs
+++ CraftingGUI.cs
@@ -53,7 +53,7 @@ namespace MagicStorage
 		private static UIElement topBar;
 
 		// TODO take a look at Terraria's UISearchBar
-		private static UI.UISearchBar searchBar;
+		public static UI.UISearchBar searchBar;
 		private static UIButtonChoice sortButtons;
 		internal static UIButtonChoice recipeButtons;
 		private static UIElement topBar2;
