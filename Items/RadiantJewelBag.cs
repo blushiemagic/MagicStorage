e593baf3fc9b9710b0b8c7dc7114927a296046ca
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-05-16 18:18:59 +0200

    Fix tml analyzer warnings

--- Items/RadiantJewelBag.cs
+++ Items/RadiantJewelBag.cs
@@ -8,7 +8,7 @@ namespace MagicStorage.Items
 	{
 		public override void OpenVanillaBag(string context, Player player, int arg)
 		{
-			if (context == "bossBag" && arg == ItemID.MoonLordBossBag && Main.rand.Next(10) == 0)
+			if (context == "bossBag" && arg == ItemID.MoonLordBossBag && Main.rand.NextBool(10))
 			{
 				var source = player.GetSource_OpenItem(ItemID.MoonLordBossBag);
 				player.QuickSpawnItem(source, ModContent.ItemType<RadiantJewel>());
