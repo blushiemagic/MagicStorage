ea2ac5cf2e89b2beab3b9bf73a0663fabd7b143c
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-06-12 22:15:46 +0200

    Fix sort issue in a different way

--- CraftingGUI.cs
+++ CraftingGUI.cs
@@ -1064,7 +1064,8 @@ namespace MagicStorage
 
 				void DoFiltering()
 				{
-					var filteredRecipes = ItemSorter.GetRecipes(sortMode, filterMode, modFilterIndex, searchBar.Text)
+					var (filteredRecipes, sortFunction) = ItemSorter.GetRecipes(sortMode, filterMode, modFilterIndex, searchBar.Text);
+					filteredRecipes = filteredRecipes
 						// show only blacklisted recipes only if choice = 2, otherwise show all other
 						.Where(x => recipeButtons.Choice == RecipeButtonsBlacklistChoice == hiddenRecipes.Contains(x.createItem.type))
 						// show only favorited items if selected
@@ -1072,15 +1073,17 @@ namespace MagicStorage
 						// hard check if this item can be crafted from available items and their recursive products
 						.Where(x => !wasItemChecklistRetrieved || IsKnownRecursively(x, availableItemsMutable, temp, tempCache))
 						// favorites first
-						.AsSequential()
-						.OrderBy(x => favorited.Contains(x.createItem.type) ? 0 : 1);
+						.OrderBy(x => favorited.Contains(x.createItem.type) ? 0 : 1)
+						.ThenBy(x => x.createItem, sortFunction)
+						.ThenBy(x => x.createItem.type)
+						.ThenBy(x => x.createItem.value);
 
 					recipes.Clear();
 					recipeAvailable.Clear();
 
 					if (recipeButtons.Choice == RecipeButtonsAvailableChoice)
 					{
-						recipes.AddRange(filteredRecipes.AsParallel().AsOrdered().Where(r => IsAvailable(r)));
+						recipes.AddRange(filteredRecipes.Where(r => IsAvailable(r)));
 						recipeAvailable.AddRange(Enumerable.Repeat(true, recipes.Count));
 					}
 					else
