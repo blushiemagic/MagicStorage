ccde3cc48f9d31a5ccd77fbe357316128edd9015
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-06-20 01:29:41 +0200

    Follow formatting style

--- Sorting/SortingCache.cs
+++ Sorting/SortingCache.cs
@@ -5,74 +5,83 @@ using Terraria.ID;
 
 #nullable enable
 
-namespace MagicStorage.Sorting {
-	/// <summary>
-	///     A helper class that stores a cache of sorting order for every item ID
-	/// </summary>
-	public static class SortingCache {
-		public static readonly SortingCacheDictionary dictionary = new();
+namespace MagicStorage.Sorting;
+
+/// <summary>
+///     A helper class that stores a cache of sorting order for every item ID
+/// </summary>
+public static class SortingCache
+{
+	public static readonly SortingCacheDictionary dictionary = new();
+}
+
+public class SortingCacheDictionary
+{
+	private class Entry
+	{
+		public int[]? IndexByType { get; }
+
+		public Entry(int[]? indexByType = null)
+		{
+			IndexByType = indexByType;
+		}
+
+		public int FindIndex(int itemType)
+		{
+			if (IndexByType is null)
+				return -1;
+
+			return itemType < 0 || itemType >= IndexByType.Length ? -1 : IndexByType[itemType];
+		}
 	}
 
-	public class SortingCacheDictionary {
-		private class Entry {
-			public int[]? IndexByType { get; }
+	private readonly Dictionary<SortMode, Entry> cache = new();
 
-			public Entry(int[]? indexByType = null) {
-				IndexByType = indexByType;
-			}
+	public int FindIndex(SortMode mode, int itemType) => cache.TryGetValue(mode, out var entry) ? entry.FindIndex(itemType) : -1;
 
-			public int FindIndex(int itemType) {
-				if (IndexByType is null)
-					return -1;
+	public void Fill()
+	{
+		cache.Clear();
 
-				return itemType < 0 || itemType >= IndexByType.Length ? -1 : IndexByType[itemType];
-			}
+		cache[SortMode.Default] = Create(SortMode.Default);
+		cache[SortMode.Id] = Create(SortMode.Id);
+		cache[SortMode.Name] = Create(SortMode.Name);
+		//These two (value and dps) are variable on item stats, but having a baseline will make the actual sorting slightly more efficient
+		cache[SortMode.Value] = Create(SortMode.Value);
+		cache[SortMode.Dps] = Create(SortMode.Dps);
+		cache[SortMode.AsIs] = new();
+	}
+
+	private static Entry Create(SortMode mode)
+	{
+		var items = ContentSamples.ItemsByType
+			.Select((pair, i) => (item: pair.Value, type: i))
+			.OrderBy(x => x.item, ItemSorter.MakeSortFunction(mode))
+			.ToArray();
+
+		int[] indices = new int[items.Length];
+
+		for (int i = 0; i < items.Length; i++)
+		{
+			(Item item, int type) = items[i];
+
+			indices[type] = item.IsAir ? -1 : i;
 		}
 
-		private readonly Dictionary<SortMode, Entry> cache = new();
+		return new Entry(indices);
+	}
 
-		public int FindIndex(SortMode mode, int itemType) => cache.TryGetValue(mode, out var entry) ? entry.FindIndex(itemType) : -1;
+	/// <summary>
+	///     Sorts the items based on the cached item order
+	/// </summary>
+	/// <returns>A sorted collection</returns>
+	public IOrderedEnumerable<Item> SortFuzzy(IEnumerable<Item> items, SortMode mode)
+	{
+		Entry entry = cache[mode];
 
-		public void Fill() {
-			cache.Clear();
+		if (entry.IndexByType is null)
+			return items.OrderBy(_ => 1); //Preserve item order
 
-			cache[SortMode.Default] = Create(SortMode.Default);
-			cache[SortMode.Id] = Create(SortMode.Id);
-			cache[SortMode.Name] = Create(SortMode.Name);
-			//These two (value and dps) are variable on item stats, but having a baseline will make the actual sorting slightly more efficient
-			cache[SortMode.Value] = Create(SortMode.Value);
-			cache[SortMode.Dps] = Create(SortMode.Dps);
-			cache[SortMode.AsIs] = new();
-		}
-
-		private static Entry Create(SortMode mode) {
-			var items = ContentSamples.ItemsByType
-				.Select((pair, i) => (item: pair.Value, type: i))
-				.OrderBy(x => x.item, ItemSorter.MakeSortFunction(mode))
-				.ToArray();
-
-			int[] indices = new int[items.Length];
-
-			for (int i = 0; i < items.Length; i++) {
-				(Item item, int type) = items[i];
-
-				indices[type] = item.IsAir ? -1 : i;
-			}
-
-			return new Entry(indices);
-		}
-
-		/// <summary>
-		///     Sorts the items based on the cached item order
-		/// </summary>
-		/// <returns>A sorted collection</returns>
-		public IOrderedEnumerable<Item> SortFuzzy(IEnumerable<Item> items, SortMode mode) {
-			Entry entry = cache[mode];
-
-			if (entry.IndexByType is null)
-				return items.OrderBy(_ => 1); //Preserve item order
-
-			return items.OrderBy(i => entry.FindIndex(i.type));
-		}
+		return items.OrderBy(i => entry.FindIndex(i.type));
 	}
 }
