a801468912eedaac73650243ab35baa0ce9c7f0e
Exterminator <kristijan.kos99@gmail.com>
2022-07-10 20:01:35 +0200

    1.4 fix recipe drawing (#196)
    * Move stuff out of `InterfaceHelper`
    
    * Change where we hide the vanilla craft menu
    
    * Make draw method void since we always return true
    
    * Invert conditions so there's less indentation
    
    * Add nullability

--- Common/Systems/MagicUI.cs
+++ Common/Systems/MagicUI.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using MagicStorage.Components;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Terraria.UI;
+
+#nullable enable
 
 namespace MagicStorage.Common.Systems;
 
@@ -9,16 +13,45 @@ public class MagicUI : ModSystem
 {
 	public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
 	{
-		InterfaceHelper.ModifyInterfaceLayers(layers);
+		int inventoryIndex = layers.FindIndex(layer => layer.Name == "Vanilla: Inventory");
+		if (inventoryIndex != -1)
+			layers.Insert(inventoryIndex + 1, new LegacyGameInterfaceLayer("MagicStorage: StorageAccess", () =>
+			{
+				DrawStorageGUI();
+				return true;
+			}, InterfaceScaleType.UI));
+	}
+
+	private static void DrawStorageGUI()
+	{
+		Player player = Main.LocalPlayer;
+		StoragePlayer modPlayer = player.GetModPlayer<StoragePlayer>();
+		Point16 storageAccess = modPlayer.ViewingStorage();
+		if (!Main.playerInventory || storageAccess.X < 0 || storageAccess.Y < 0)
+			return;
+
+		ModTile modTile = TileLoader.GetTile(Main.tile[storageAccess.X, storageAccess.Y].TileType);
+		if (modTile is not StorageAccess access)
+			return;
+
+		TEStorageHeart heart = access.GetHeart(storageAccess.X, storageAccess.Y);
+		if (heart is null)
+			return;
+
+		Main.hidePlayerCraftingMenu = true;
+
+		if (access is EnvironmentAccess)
+			EnvironmentGUI.Draw();
+		else if (access is CraftingAccess)
+			CraftingGUI.Draw();
+		else
+			StorageGUI.Draw();
 	}
 
 	public override void PostUpdateInput()
 	{
 		if (!Main.instance.IsActive)
 			return;
-
-		if (!Main.gameMenu && StoragePlayer.LocalPlayer.ViewingStorage().X >= 0)
-			Main.hidePlayerCraftingMenu = true;
 
 		StorageGUI.Update(null);
 		CraftingGUI.Update(null);
