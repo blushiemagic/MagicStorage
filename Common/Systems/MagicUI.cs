ec4d34fd143e75ee3e35ac4894112632cfe999a9
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-07-10 19:50:34 +0200

    Move stuff out of `InterfaceHelper`

--- Common/Systems/MagicUI.cs
+++ Common/Systems/MagicUI.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using MagicStorage.Components;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Terraria.UI;
 
@@ -9,7 +11,35 @@ public class MagicUI : ModSystem
 {
 	public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
 	{
-		InterfaceHelper.ModifyInterfaceLayers(layers);
+		int inventoryIndex = layers.FindIndex(layer => layer.Name == "Vanilla: Inventory");
+		if (inventoryIndex != -1)
+			layers.Insert(inventoryIndex + 1, new LegacyGameInterfaceLayer("MagicStorage: StorageAccess", DrawStorageGUI, InterfaceScaleType.UI));
+	}
+
+	private static bool DrawStorageGUI()
+	{
+		Player player = Main.LocalPlayer;
+		StoragePlayer modPlayer = player.GetModPlayer<StoragePlayer>();
+		Point16 storageAccess = modPlayer.ViewingStorage();
+		if (Main.playerInventory && storageAccess.X >= 0 && storageAccess.Y >= 0)
+		{
+			ModTile modTile = TileLoader.GetTile(Main.tile[storageAccess.X, storageAccess.Y].TileType);
+			if (modTile is StorageAccess access)
+			{
+				TEStorageHeart heart = access.GetHeart(storageAccess.X, storageAccess.Y);
+				if (heart is not null)
+				{
+					if (access is EnvironmentAccess)
+						EnvironmentGUI.Draw();
+					else if (access is CraftingAccess)
+						CraftingGUI.Draw();
+					else
+						StorageGUI.Draw();
+				}
+			}
+		}
+
+		return true;
 	}
 
 	public override void PostUpdateInput()
--- InterfaceHelper.cs
+++ InterfaceHelper.cs
@@ -22,39 +22,6 @@ namespace MagicStorage
 				throw new Exception("Reflection value was null (source: InterfaceHelper.Initialize)");
 		}
 
-		public static void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-		{
-			int inventoryIndex = layers.FindIndex(layer => layer.Name == "Vanilla: Inventory");
-			if (inventoryIndex != -1)
-				layers.Insert(inventoryIndex + 1, new LegacyGameInterfaceLayer("MagicStorage: StorageAccess", DrawStorageGUI, InterfaceScaleType.UI));
-		}
-
-		public static bool DrawStorageGUI()
-		{
-			Player player = Main.LocalPlayer;
-			StoragePlayer modPlayer = player.GetModPlayer<StoragePlayer>();
-			Point16 storageAccess = modPlayer.ViewingStorage();
-			if (Main.playerInventory && storageAccess.X >= 0 && storageAccess.Y >= 0)
-			{
-				ModTile modTile = TileLoader.GetTile(Main.tile[storageAccess.X, storageAccess.Y].TileType);
-				if (modTile is StorageAccess access)
-				{
-					TEStorageHeart heart = access.GetHeart(storageAccess.X, storageAccess.Y);
-					if (heart is not null)
-					{
-						if (access is EnvironmentAccess)
-							EnvironmentGUI.Draw();
-						else if (access is CraftingAccess)
-							CraftingGUI.Draw();
-						else
-							StorageGUI.Draw();
-					}
-				}
-			}
-
-			return true;
-		}
-
 		public static void HideItemIconCache()
 		{
 			_itemIconCacheTimeInfo.SetValue(null, 0);
