5590ab15e82c211a276e4f1d394fdb587eaf58c5
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-07-10 18:21:24 +0200

    Cache items used in Creative Storage

--- Components/TECreativeStorageUnit.cs
+++ Components/TECreativeStorageUnit.cs
@@ -1,91 +1,67 @@
-using System.Collections;
 using System.Collections.Generic;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace MagicStorage.Components
+#nullable enable
+
+namespace MagicStorage.Components;
+
+public class TECreativeStorageUnit : TEAbstractStorageUnit
 {
-	public class TECreativeStorageUnit : TEAbstractStorageUnit
+	private static Item AirItem = null!;
+	private static Item[] Items = null!;
+
+	public override void Load(Mod mod)
 	{
-		public override bool IsFull => true;
+		base.Load(mod);
 
-		public override bool ValidTile(in Tile tile) => tile.TileType == ModContent.TileType<CreativeStorageUnit>() && tile.TileFrameX == 0 && tile.TileFrameY == 0;
-
-		public override bool HasSpaceInStackFor(Item check) => false;
-
-		public override bool HasItem(Item check, bool ignorePrefix = false) => !Inactive;
-
-		public override IEnumerable<Item> GetItems() => new CreativeEnumerable(Inactive);
-
-		public override void DepositItem(Item toDeposit)
+		AirItem = new();
+		Items = new Item[ItemLoader.ItemCount];
+		for (int i = 0; i < Items.Length; i++)
 		{
-		}
-
-		public override Item TryWithdraw(Item lookFor, bool locked = false, bool keepOneIfFavorite = false)
-		{
-			if (Inactive)
-				return new Item();
-			return lookFor.Clone();
+			var item = Items[i] = new Item(i);
+			item.stack = item.maxStack;
 		}
 	}
 
-	internal class CreativeEnumerable : IEnumerable<Item>
+	public override void Unload()
 	{
-		private readonly bool inactive;
+		base.Unload();
 
-		internal CreativeEnumerable(bool inactive)
-		{
-			this.inactive = inactive;
-		}
-
-		public IEnumerator<Item> GetEnumerator() => new CreativeEnumerator(inactive);
-
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		AirItem = null!;
+		Items = null!;
 	}
 
-	internal class CreativeEnumerator : IEnumerator<Item>
+	public override bool IsFull => true;
+
+	public override bool ValidTile(in Tile tile) => tile.TileType == ModContent.TileType<CreativeStorageUnit>() && tile.TileFrameX == 0 && tile.TileFrameY == 0;
+
+	public override bool HasSpaceInStackFor(Item check) => false;
+
+	public override bool HasItem(Item check, bool ignorePrefix = false) => !Inactive;
+
+	public override IEnumerable<Item> GetItems()
 	{
-		private readonly bool inactive;
-		private int id;
-
-		internal CreativeEnumerator(bool inactive)
+		for (int i = 0; i < Items.Length; i++)
 		{
-			this.inactive = inactive;
+			var item = Items[i];
+			if (item.IsAir)
+				item.SetDefaults(i);
+
+			item.stack = item.maxStack;
+			yield return item;
 		}
+	}
 
-		public Item Current
-		{
-			get
-			{
-				Item item = new();
-				item.SetDefaults(id, true);
-				item.stack = item.maxStack;
-				return item;
-			}
-		}
+	public override void DepositItem(Item toDeposit)
+	{
+	}
 
-		object IEnumerator.Current => Current;
+	public override Item TryWithdraw(Item lookFor, bool locked = false, bool keepOneIfFavorite = false)
+	{
+		if (Inactive)
+			return AirItem;
 
-		public bool MoveNext()
-		{
-			if (inactive)
-				return false;
-			do
-			{
-				id++;
-			} while (id < ItemID.Sets.Deprecated.Length && ItemID.Sets.Deprecated[id]);
-
-			return id < ItemID.Sets.Deprecated.Length;
-		}
-
-		public void Reset()
-		{
-			id = 0;
-		}
-
-		public void Dispose()
-		{
-		}
+		return lookFor.Clone();
 	}
 }
