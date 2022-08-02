0ec7aac5e31356853fb58c98ef8b54b29181efec
absoluteAquarian <jruss88@me.com>
2022-08-02 17:19:09 -0500

    remove unnecessary defines

--- Items/RadiantJewelBag.cs
+++ Items/RadiantJewelBag.cs
@@ -7,19 +7,6 @@ namespace MagicStorage.Items
 {
 	public class RadiantJewelBag : GlobalItem
 	{
-#if TML_2022_6
-		public override void OpenVanillaBag(string context, Player player, int arg)
-		{
-			//18% chance to drop 1 item in Expert Mode
-			//25% chance to drop 1 item in Master Mode
-			float chance = Main.masterMode ? 0.25f : 0.18f;
-			if (context == "bossBag" && arg == ItemID.MoonLordBossBag && Main.rand.NextFloat() < chance)
-			{
-				var source = player.GetSource_OpenItem(ItemID.MoonLordBossBag);
-				player.QuickSpawnItem(source, ModContent.ItemType<RadiantJewel>());
-			}
-		}
-#else
 		public override void ModifyItemLoot(Item item, ItemLoot itemLoot) {
 			//18% chance to drop 1 item in Expert Mode
 			//25% chance to drop 1 item in Master Mode
@@ -36,10 +23,8 @@ namespace MagicStorage.Items
 				maximumDropped: 1,
 				chanceNumerator: 1));
 		}
-#endif
 	}
 
-#if !TML_2022_6
 	public class IsExpertNotMaster : IItemDropRuleCondition {
 		public bool CanDrop(DropAttemptInfo info) => Main.expertMode && !Main.masterMode;
 
@@ -47,5 +32,4 @@ namespace MagicStorage.Items
 
 		public string GetConditionDescription() => new Conditions.IsExpert().GetConditionDescription();
 	}
-#endif
 }
