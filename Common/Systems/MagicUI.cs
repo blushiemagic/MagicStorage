9ae83b6a579eaa6390f5404a52ab220fb96763f8
absoluteAquarian <jruss88@me.com>
2022-08-03 11:38:46 -0500

    forgot to actually implement EnvironmentUIState

--- Common/Systems/MagicUI.cs
+++ Common/Systems/MagicUI.cs
@@ -24,7 +24,7 @@ public class MagicUI : ModSystem
 		uiInterface = new();
 		craftingUI = new CraftingUIState();
 		storageUI = new StorageUIState();
-		// environmentUI = new EnvironmentUIState();
+		environmentUI = new EnvironmentUIState();
 	}
 
 	public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
