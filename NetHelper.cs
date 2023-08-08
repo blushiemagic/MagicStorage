34675e2fd87078ef6f35e4d7ba8e9a1039f795e6
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-08-08 02:02:24 -0500

    fix typo bug which caused craft result item withdraw to fail (response packet wasn't sent back to the client)
    (cherry picked from commit c803b83c4c63c0a629b5c9727d0eceeac3a0e3f5)

--- NetHelper.cs
+++ NetHelper.cs
@@ -384,6 +384,10 @@ namespace MagicStorage
 				{
 					_ = reader.ReadInt32();
 				}
+				else if (op == TEStorageHeart.Operation.WithdrawThenTryModuleInventory || op == TEStorageHeart.Operation.WithdrawToInventoryThenTryModuleInventory)
+				{
+					_ = ItemIO.Receive(reader, true, true);
+				}
 
 				return;
 			}
