2313a03caf7ed04b720cf34d67b84c76375995a4
absoluteAquarian <30454621+absoluteAquarian@users.noreply.github.com>
2023-11-21 15:35:22 -0600

    recipe group name standardization

--- Common/Systems/MagicRecipes.cs
+++ Common/Systems/MagicRecipes.cs
@@ -51,52 +51,64 @@ namespace MagicStorage.Common.Systems {
 			int[] items = GetItems(ItemID.Chest, chestItemRegex);
 			RecipeGroup group = new(() => $"{any} Chest", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyChest", group);
+			RegisterGroupClone(group, nameof(ItemID.Chest));
 
 			items = new int[] { ItemID.SnowBlock, ItemID.IceBlock, ItemID.PurpleIceBlock, ItemID.PinkIceBlock, ItemID.RedIceBlock };
 			group = new RecipeGroup(() => $"{any} {Language.GetTextValue("Mods.MagicStorage.SnowBiomeBlock")}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnySnowBiomeBlock", group);
+			RegisterGroupClone(group, nameof(ItemID.SnowBlock));
 
 			items = new[] { ItemID.Diamond, ModContent.ItemType<ShadowDiamond>() };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.Diamond)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyDiamond", group);
+			RegisterGroupClone(group, nameof(ItemID.Diamond));
 
 			items = GetItems(ItemID.WorkBench, workBenchItemRegex,
 				ItemID.HeavyWorkBench);
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.WorkBench)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyWorkBench", group);
+			RegisterGroupClone(group, nameof(ItemID.WorkBench));
 
 			items = new int[] { ItemID.IronAnvil, ItemID.LeadAnvil };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.IronAnvil)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyPreHmAnvil", group);
+			RegisterGroupClone(group, nameof(ItemID.IronAnvil));
 
 			items = new int[] { ItemID.Bottle, ItemID.PinkVase, ItemID.Mug, ItemID.DynastyCup, ItemID.WineGlass, ItemID.HoneyCup, ItemID.SteampunkCup };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.Bottle)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyBottle", group);
+			RegisterGroupClone(group, nameof(ItemID.Bottle));
 
 			items = GetItems(ItemID.MetalSink, sinkItemRegex);
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.MetalSink)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnySink", group);
+			RegisterGroupClone(group, nameof(ItemID.MetalSink));
 
 			items = GetItems(ItemID.WoodenTable, tableItemRegex,
 				ItemID.BewitchingTable, ItemID.AlchemyTable);
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.WoodenTable)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyTable", group);
+			RegisterGroupClone(group, nameof(ItemID.WoodenTable));
 
 			items = new int[] { ItemID.CookingPot, ItemID.Cauldron };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.CookingPot)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyCookingPot", group);
+			RegisterGroupClone(group, nameof(ItemID.CookingPot));
 
 			items = new int[] { ItemID.MythrilAnvil, ItemID.OrichalcumAnvil };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.MythrilAnvil)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyHmAnvil", group);
+			RegisterGroupClone(group, nameof(ItemID.MythrilAnvil));
 
 			items = new int[] { ItemID.AdamantiteForge, ItemID.TitaniumForge };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.AdamantiteForge)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyHmFurnace", group);
+			RegisterGroupClone(group, nameof(ItemID.AdamantiteForge));
 
 			items = GetItems(ItemID.Bookcase, bookcaseItemRegex);
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.Bookcase)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyBookcase", group);
+			RegisterGroupClone(group, nameof(ItemID.Bookcase));
 
 			items = new int[]
 			{
@@ -114,10 +126,12 @@ namespace MagicStorage.Common.Systems {
 			};
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.Tombstone)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyTombstone", group);
+			RegisterGroupClone(group, nameof(ItemID.Tombstone));
 
 			items = GetItems(ItemID.Campfire, campfireItemRegex);
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.Campfire)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyCampfire", group);
+			RegisterGroupClone(group, nameof(ItemID.Campfire));
 
 			items = new[] { ModContent.ItemType<DemonAltar>(), ModContent.ItemType<CrimsonAltar>() };
 
@@ -141,16 +155,33 @@ namespace MagicStorage.Common.Systems {
 			items = new int[] { ItemID.SilverBar, ItemID.TungstenBar };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.SilverBar)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnySilverBar", group);
+			RegisterGroupClone(group, nameof(ItemID.SilverBar));
 
 			items = new int[] { ItemID.MythrilBar, ItemID.OrichalcumBar };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.MythrilBar)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyMythrilBar", group);
+			RegisterGroupClone(group, nameof(ItemID.MythrilBar));
 
 			items = new int[] { ItemID.DemoniteBar, ItemID.CrimtaneBar };
 			group = new RecipeGroup(() => $"{any} {Lang.GetItemNameValue(ItemID.DemoniteBar)}", items);
 			RecipeGroup.RegisterGroup("MagicStorage:AnyDemoniteBar", group);
+			RegisterGroupClone(group, nameof(ItemID.DemoniteBar));
 
 			ModLoadingProgressHelper.SetLoadingSubProgressText("");
+		}
+
+		private static void RegisterGroupClone(RecipeGroup original, string groupName) {
+			// If the group already exists, union the sets and overwrite the reference
+			// Otherwise, make a new group that's a copy of the original group
+			if (RecipeGroup.recipeGroupIDs.TryGetValue(groupName, out int groupID)) {
+				RecipeGroup group = RecipeGroup.recipeGroups[groupID];
+				original.ValidItems.UnionWith(group.ValidItems);
+				group.ValidItems = original.ValidItems;
+			} else {
+				RecipeGroup group = new RecipeGroup(original.GetText, new int[1]);
+				group.ValidItems = original.ValidItems;
+				RecipeGroup.RegisterGroup(groupName, group);
+			}
 		}
 	}
 }
